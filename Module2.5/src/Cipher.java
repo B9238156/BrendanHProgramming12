e1a93a38c72707d1be3f02b80094ff17be564e51
B9238156 <151875764+B9238156@users.noreply.github.com>
2024-01-25 20:47:43 -0800

    Add files via upload

---
+++ Module2.5/src/Cipher.java
@@ -0,0 +1,41 @@
+import javax.sound.midi.SysexMessage;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Map;
+
+
+public class Cipher {
+    public HashMap<Integer, Character> alphabet = new HashMap<>();
+    public int shift;
+
+    public Cipher(int shift) {
+        this.shift = shift;
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < 26; i++) {
+            alphabet.put(i, alpha.charAt(i));
+        }
+    }
+
+    public String encrypt(String message) {
+
+        String encryptedMessage = "";
+        for (int p = 0; p < message.length(); p++) {
+            // iterate each entry of hashmap
+            for (Entry<Integer, Character> entry : this.alphabet.entrySet()) {
+                //System.out.println(entry.getValue());
+                // if give value is equal to value from entry
+                // print the corresponding key
+                if (entry.getValue() == message.charAt(p)) {
+                    //System.out.println("The key for value " + message.charAt(p) + " is " + entry.getKey());
+                    int position = entry.getKey();
+                    int shiftSpot = (position + this.shift) % 26;
+                    //System.out.println(shiftSpot);
+                    //System.out.println(alphabet.get(shiftSpot));
+                    encryptedMessage = encryptedMessage + alphabet.get(shiftSpot);
+                    break;
+                }
+            }
+        }
+        return encryptedMessage;
+    }
+}
