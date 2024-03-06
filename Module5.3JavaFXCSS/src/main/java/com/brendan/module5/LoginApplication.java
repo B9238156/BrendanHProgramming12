76465e12ad93edad4704b81b063859e3eb4dd871
B9238156 <151875764+B9238156@users.noreply.github.com>
2024-03-06 12:14:51 -0800

    Base Code - Fixed Base Code

---
+++ Module5.3JavaFXCSS/src/main/java/com/brendan/module5/LoginApplication.java
@@ -0,0 +1,25 @@
+package com.brendan.module5;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class LoginApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Brendan's IT");
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Module5.3JavaFXCSS/src/main/java/com/brendan/module5/LoginController.java
@@ -0,0 +1,60 @@
+package com.brendan.module5;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class LoginController {
+
+        @FXML
+        private TextField password;
+
+
+        @FXML
+        private TextField welcome;
+
+        @FXML
+        private TextField userName;
+
+        public void exitSystem() {
+            System.exit(1);
+        }
+
+        public void login() throws IOException {
+            System.out.println("this will log me in with an id of " + userName.getText() + " and a password of " + password.getText());
+            createNewWindow(userName.getText());
+            //System.exit(1);
+        }
+
+        public void createNewWindow(String userName) throws IOException {
+            // Create a new stage (window)
+            Stage newWindow = new Stage();
+            newWindow.setTitle("New Window");
+
+            // Load the FXML document
+            Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+
+            // Set the scene
+            Scene scene = new Scene(root);
+            newWindow.setScene(scene);
+
+            TextField welcomeTxt = (TextField) root.lookup("#welcome");
+            welcomeTxt.setText(userName);
+
+
+            // Display the stage
+            newWindow.show();
+        }
+
+}
